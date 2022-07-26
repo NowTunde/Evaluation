ded8a090f6c7acfaacebb26022ea39457d1e428b
Tunde Babayemi <tundebabayemi@Tundes-MBP.hsd1.ga.comcast.net>
2022-07-26 05:21:50 -0400

    Initial Checkin.

---
+++ TwitterStatistics.DomainServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TwitterStatistics.DomainServices
+{
+    public class Class1
+    {
+    }
+}
---
+++ TwitterStatistics.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TwitterStatisticsApi.Repository.Interfaces
+{
+    public interface IRepository
+    {
+        Task GetTweetAsync();
+        Task GetTweetsAsync();
+    }
+}
